bfbf1240f50d7eecd872b131c7e82f9f93e8b504
itsRichard <rtl@uw.edu>
2016-04-24 16:34:25 -0700

    Resilience added to music player

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+	static MusicPlayer instance = null;
+
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Loading BGM");
-		GameObject.DontDestroyOnLoad (gameObject);
+		
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player destroyed");
+		} 
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}		
 	}
 	
 	// Update is called once per frame
