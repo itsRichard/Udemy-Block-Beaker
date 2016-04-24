3db2e0e3ffca58357f39c27b3a8502e677ca4e48
itsRichard <rtl@uw.edu>
2016-04-24 15:48:47 -0700

    Menu system imported and implemented
    Menu system from Number Wizard UI

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel (string name){
+		Debug.Log ("Load Level Request: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Quit Requested");
+		Application.Quit();
+	}
+
+}
