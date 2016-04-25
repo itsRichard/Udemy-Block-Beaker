f8c1dd41fde86295705b48d4cecb0be3582c1575
itsRichard <rtl@uw.edu>
2016-04-25 13:54:39 -0700

    Tidy up

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class LoseCollider : MonoBehaviour {
 
+	public LevelManager LevelManager;
 	void OnCollisionEnter2D (Collision2D collision) {
 		print("Collision");
 	}
 	
 	void OnTriggerEnter2D (Collider2D trigger){
 		print("Trigger");
+		LevelManager.LoadLevel ("Win");
+		
+		
 		}
 
 }
